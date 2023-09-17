0db55ddbbff98391938b17af8914d26cc6e4d5c5
gabeFrancisco <gabegit2@gmail.com>
2023-09-17 15:08:24 -0300

    created table for dynamic data]

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function btnAlert() {
-  const input = document.getElementById("dataValue").value
-  alert(input);
+  const input = document.getElementById("dataValue");
+  alert(input.value);
+  // input.style.padding = '2rem'
+
+    let size = 1
+    var interval = setInterval(() => {
+      const list = document.getElementById("someList");
+      const li = document.createElement("li");
+      li.innerHTML = input.value;
+      li.style.fontSize = `${size}px`
+      size++
+      list.appendChild(li);
+    }, 1000)
 }

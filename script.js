0833ad567f7c84c47083b7a6e41abaf71a560d9c
gabeFrancisco <gabegit2@gmail.com>
2023-09-17 15:17:00 -0300

    added clear button for world generation

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+let interval;
+let list = document.getElementById("someList");
+
 function btnAlert() {
   const input = document.getElementById("dataValue");
   alert(input.value);
   // input.style.padding = '2rem'
 
-    let size = 1
-    var interval = setInterval(() => {
-      const list = document.getElementById("someList");
-      const li = document.createElement("li");
-      li.innerHTML = input.value;
-      li.style.fontSize = `${size}px`
-      size++
-      list.appendChild(li);
-    }, 1000)
+  let size = 1;
+  interval = setInterval(() => {
+    const li = document.createElement("li");
+    li.innerHTML = input.value;
+    li.style.fontSize = `${size}px`;
+    size++;
+    list.appendChild(li);
+  }, 1000);
+}
+
+function worldClear() {
+  clearInterval(interval);
+  list.innerHTML = "";
 }
